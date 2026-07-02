605016f324f8a07015c385c04f66b83db775386f
yash <jagtapyash1761@gmail.com>
2026-07-02 15:02:16 +0530

    Created Box and Product models

--- box_selector/models.py
+++ box_selector/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    # Dimensions can be stored in cm/inches (ensure you enforce a standard unit across your app)
+    length = models.DecimalField(max_digits=10, decimal_places=2)
+    width = models.DecimalField(max_digits=10, decimal_places=2)
+    height = models.DecimalField(max_digits=10, decimal_places=2)
+    # Weight can be stored in kg/lbs
+    weight = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} ({self.length}x{self.width}x{self.height}, {self.weight})"
+
+
+class Box(models.Model):
+    name = models.CharField(max_length=255)
+    internal_length = models.DecimalField(max_digits=10, decimal_places=2)
+    internal_width = models.DecimalField(max_digits=10, decimal_places=2)
+    internal_height = models.DecimalField(max_digits=10, decimal_places=2)
+    max_weight_capacity = models.DecimalField(max_digits=10, decimal_places=2)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} (Max Wt: {self.max_weight_capacity} | Cost: ${self.cost})"
